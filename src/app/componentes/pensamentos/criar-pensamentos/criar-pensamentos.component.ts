d414480570fabfa27b393a4a7f7c50bd345651f0
Vitor Gabriel <vitor_gha@hotmail.com>
2023-03-16 10:10:16 -0300

    memoteca

---
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -0,0 +1,28 @@
+import { Component, NgModule , OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-criar-pensamentos',
+  templateUrl: './criar-pensamentos.component.html',
+  styleUrls: ['./criar-pensamentos.component.css']
+})
+export class CriarPensamentosComponent {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular.',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  criarPensamento(){
+    alert('Novo pensamento criado.')
+  }
+
+  cancelarPensamento(){
+
+    alert('Pensamento cancelaro!')
+  }
+
+
+}
